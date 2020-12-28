b5c062b222e3af6e8e956bba1fa0a14d8205dfe5
suguru prathyusha <72124136+Prathyusha-Suguru@users.noreply.github.com>
2020-12-29 00:10:56 +0530

    version 3.0

---
+++ Board.java
@@ -0,0 +1,48 @@
+public class Board extends Cell{
+	public int p,q;
+	public boolean array[][];
+	public Board(int p,int q) {
+		this.array=new boolean[p][q];
+		this.p=p;
+		this.q=q;
+		for(int i=0;i<p;i++) {
+			for(int j=0;j<q;j++) {
+				this.array[i][j]=false;
+			}
+		}
+	}
+	public void createBoard(int l[][]){                      
+		for (int i =0; i <l.length;i++) {                                          
+			int row = l[i][0];
+			int column = l[i][1];
+			this.array[row][column] = true;
+		}                                                               
+	}
+	public boolean[][] requiredGeneration(boolean l[][],int  k ) {                                                
+		boolean[][] newBoard=new boolean[p][q]; 
+		for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
+                int ilives = countLiveCells(l, i, j);
+
+                if (l[i][j]) {
+                    if (ilives < 2) {                                    
+                        newBoard[i][j] = false;
+                    } else if (ilives <= 3) {
+                        newBoard[i][j] = true;
+                    } else if (ilives > 3) {
+                        newBoard[i][j] = false;
+                    }
+                } else {
+                    if (ilives == 3) {
+                        newBoard[i][j] = true;
+                    }
+                }
+            }
+		}
+		while(k>1) {													
+			newBoard=requiredGeneration(newBoard,1);
+			k--;
+		}
+		return newBoard;
+	}	
+}
